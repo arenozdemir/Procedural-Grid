0bfa7e01b92eed7853df5f47b92895cdcd3d11e1
emreozdemir <1030520973@erciyes.edu.tr>
2023-08-18 12:29:44 +0300

    Initial commit

---
+++ Assets/Scripts/Fractal.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fractal : MonoBehaviour
+{
+    [SerializeField, Range(1, 8)] int depth = 4;
+
+    private void Start()
+    {
+        name = "Fractal " + depth;
+        if (depth <= 1)
+        {
+            return;
+        }
+        Fractal child = Instantiate(this);
+        child.depth = depth - 1;
+        child.transform.SetParent(transform, false);
+        child.transform.localPosition = 0.75f * Vector3.right;
+        child.transform.localScale = Vector3.one * 0.5f;
+
+        child = Instantiate(this);
+        child.depth = depth - 1;
+        child.transform.SetParent(transform, false);
+        child.transform.localPosition = 0.75f * Vector3.up;
+        child.transform.localScale = 0.5f * Vector3.one;
+    }
+}
